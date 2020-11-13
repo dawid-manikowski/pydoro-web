c44857c53309b2b53eeb2743c3ad0b78d334651b
Dawid Manikowski <dawid.manikowski@makeitright.pl>
2020-11-14 00:14:10 +0100

    Remove indentation on policy json

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,19 +20,19 @@ resource "aws_iam_policy" "ec2_allow" {
 resource "aws_iam_role" "ec2_role" {
   name               = "ec2_role"
   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "ec2.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
-        }
-      ]
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
     }
+  ]
+}
 EOF
 }
 
